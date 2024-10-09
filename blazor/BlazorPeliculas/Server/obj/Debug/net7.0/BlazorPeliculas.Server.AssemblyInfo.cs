b7b09d2d80035447d4ef288863da476f3cc074db
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-10-08 23:27:34 -0600

    blazor -> seccion 3 c 23, 4:50 Parametros arbitrarios

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a732ed67fac6b4d94fed212815fbdd08a5980d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cc787a800515c3650c3751e546e6cc7b4dbcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- blazor/BlazorPeliculas/Shared/obj/Debug/net7.0/BlazorPeliculas.Shared.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Shared/obj/Debug/net7.0/BlazorPeliculas.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a732ed67fac6b4d94fed212815fbdd08a5980d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cc787a800515c3650c3751e546e6cc7b4dbcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
