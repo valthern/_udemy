023a8c6fb7c657bf52223e3c88ae9e16d256a405
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-12-06 18:27:39 -0600

    blazor -> seccion 5 c 64, 5:20 componente de insertar imagen

--- blazor/BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87237988b81178e340249de450ac2c0b64776c79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603e003949ba95b9b8b2de0e96ee7ebc1aacb074")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- blazor/BlazorPeliculas/BlazorPeliculas/Shared/Entidades/Actor.cs
+++ blazor/BlazorPeliculas/BlazorPeliculas/Shared/Entidades/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorPeliculas.Shared.Entidades
     public class Actor
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; } = null!;
         public string? Biografia { get; set; }
         public string? Foto { get; set; }
