67703a077f92c1d8302a9e29b39845d7fa5cd7fe
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-12-09 18:33:30 -0600

    blazor -> seccion 5 c 67 9:00 componente de seleccion multiple

--- blazor/BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603e003949ba95b9b8b2de0e96ee7ebc1aacb074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023a8c6fb7c657bf52223e3c88ae9e16d256a405")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
