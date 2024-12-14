54a929fb9bf25a0de4ae30727b2e2aed4e96a3d1
Omar I. Martínez Gómez <omar_238@hotmail.com>
2024-12-14 12:37:05 -0600

    blazor -> seccion 6 c 78 preparando el proyecto para que guarde imagenes

---
+++ blazor/BlazorPeliculas/BlazorPeliculas/Server/Controllers/ActoresController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorPeliculas.Server.Helpers;
+using BlazorPeliculas.Shared.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorPeliculas.Server.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+
+        public ActoresController(ApplicationDbContext context,IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.context = context;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Actor actor)
+        {
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            return actor.Id;
+        }
+    }
+}
