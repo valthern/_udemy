314ef61474bae1f774fbff0d6db290eb7f1f6185
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-09-09 23:08:12 -0600

    blazor -> v 24, Parametros arbitrarios

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777ba9a7ebf78610f700824b646b69abb184fdb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9a93f7faa01f96230bda96fad55c12d59fed28")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
