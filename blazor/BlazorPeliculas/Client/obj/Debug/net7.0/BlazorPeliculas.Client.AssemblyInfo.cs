93bab42fc51f3311413be10c19a2a44343ae1e0e
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-09-10 22:18:48 -0600

    blazor -> v 27, Bind Get y Set

--- blazor/BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9a93f7faa01f96230bda96fad55c12d59fed28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314ef61474bae1f774fbff0d6db290eb7f1f6185")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
