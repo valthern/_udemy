2e67ca82b08c3334d4c44c48e3d2181c7ffd8c42
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-10-17 23:29:11 -0600

    blazor -> v 54 Lazy Loading

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56deb900af9d6c6d6ad04b7a33dad5153021e970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2afaffec64ede0a667b59f1f8ca5a88d841bdb2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
