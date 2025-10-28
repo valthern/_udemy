b36f0424758b5be1d89dbd53536b9325a31913f4
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-10-27 23:19:06 -0600

    blazor -> v 59, formulario de actualizacion de Generos

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb31109d6b84a09827bf1bfb07e5a8ebad8c2da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27d80d0ec5e54ab259d70ec4450f636561a7753")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- blazor/BlazorPeliculas/Shared/Entidades/Genero.cs
+++ blazor/BlazorPeliculas/Shared/Entidades/Genero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorPeliculas.Shared.Entidades
     public class Genero
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
         public string Nombre { get; set; }
     }
 }
