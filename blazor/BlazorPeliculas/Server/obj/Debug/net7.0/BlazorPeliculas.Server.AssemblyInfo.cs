5119060afc97f0fa8e2b7d0ddcbdfadcba66e2dc
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-11-06 23:05:41 -0600

    blazor -> v 63, Formulario de Actores

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55caecce057670b52dcc1fca30f102d650d2648b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25abd1f41804650c825c187b60f7f9873f326eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
