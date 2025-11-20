e1c138e4a663ed0bf6cc8323da0f0235497c9d0b
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-11-19 22:53:25 -0600

    blazor -> v 68, Componente seleccion multiple - Typeahead (autocomplete) - Arrastrar elementos

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb2f272a80043448f55a23895f015249ffbaeff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f562c0cf09f3307d932a6cffcc769ea6b9d291a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
