ef51aaf2ff02722f39ed75aed7be9a8bafe5f3af
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-11-21 23:17:12 -0600

    blazor -> v 73, Configurando entidades

--- blazor/BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c138e4a663ed0bf6cc8323da0f0235497c9d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac8229624cd68650f5675b52a41c0026e20eb0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ blazor/BlazorPeliculas/Server/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorPeliculas.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
