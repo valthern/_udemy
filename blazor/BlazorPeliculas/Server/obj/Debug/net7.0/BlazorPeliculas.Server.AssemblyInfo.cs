0c6371df45774fef0415dd58d268d955d199e93f
Omar I. Martínez Gómez <omar_238@hotmail.com>
2026-01-16 18:32:10 -0600

    blazor -> v 125, Migrando la app - Entidades - Usuarios

--- blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ blazor/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b82ce252593591c2dd1b1314678ff32833a863c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160708bdc256a3133cb37d4c36c93cd32bbb63a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
