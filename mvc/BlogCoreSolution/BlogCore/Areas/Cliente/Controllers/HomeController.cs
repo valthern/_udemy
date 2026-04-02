2ca935a7956aebd8da6c9910aa41c64f8832e861
Omar I. Martínez Gómez <omar_238@hotmail.com>
2026-04-01 18:30:03 -0600

    mvc -> v 38, configuracion de bootstrap e iconos

---
+++ mvc/BlogCoreSolution/BlogCore/Areas/Cliente/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using BlogCoreSolution.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlogCore.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
