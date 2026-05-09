a82c66cbaad04b35c03e3a7ad5c560d83dabe230
Omar I. Martínez Gómez <omar_238@hotmail.com>
2026-05-08 18:30:53 -0600

    mvc -> v 11, 5:20, agregar servicio identity

---
+++ identity/ProyectoIdentity/ProyectoIdentity/Datos/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoIdentity.Datos
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // Adición de modelos personalizados que vayamos necesitando.
+
+    }
+}
---
+++ identity/ProyectoIdentity/ProyectoIdentity/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProyectoIdentity.Datos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var nombreCadCon = "csiTiDevConnection";
+var cadenaConexionBd = builder.Configuration.GetConnectionString(nombreCadCon);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(cadenaConexionBd));
+
+// Se agrega el soporte para Identity.
+builder.Services.AddIdentity<IdentityUser, IdentityRole>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
